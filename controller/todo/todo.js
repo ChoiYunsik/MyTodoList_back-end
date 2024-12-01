8f3c6aa104f35b661d6547fb7962cc68687cdee8
ChoiYunsik <ykuranus@gmail.com>
2024-12-02 00:06:05 +0900

    update

--- controller/todo/todo.js
+++ controller/todo/todo.js
@@ -68,7 +68,7 @@ const deleteTodo = async (req, res) => {
     
 }
  const updateTodoTitle = async (req, res) => {
-    console.log(req.body, value, title)
+    console.log(req.body, value)
     const {title} = req.body;
     const updatedTitle = await Todo.updateOne({
         title : title
