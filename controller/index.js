bbba7823162d9201c3b40072e132013eb7c2eccf
ChoiYunsik <ykuranus@gmail.com>
2024-12-01 22:15:32 +0900

    update v2

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,6 @@
 const index = (req, res) => {
     res.set({"Content-Type" : "text/html; charset=utf-8"})
-    res.end("<h1>Welcome Express!!😊</h1>")
+    res.end("<h1>Welcome Express!! v2😊</h1>")
 }
 
 export { index };
